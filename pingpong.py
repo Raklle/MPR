511c78957bbeeda5567e9e2c5985c6e1f17a34e6
Tomasz Janik <73713909+Raklle@users.noreply.github.com>
2026-03-13 17:26:22 +0100

    11

--- pingpong.py
+++ pingpong.py
@@ -1,25 +1,14 @@
+#!/usr/bin/env python
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-N = 10000
-msg = bytearray(1)
-
-comm.Barrier()
-start = MPI.Wtime()
-
-for i in range(N):
-    if rank == 0:
-        comm.Send(msg, dest=1)
-        comm.Recv(msg, source=1)
-    elif rank == 1:
-        comm.Recv(msg, source=0)
-        comm.Send(msg, dest=0)
-
-comm.Barrier()
-end = MPI.Wtime()
-
 if rank == 0:
-    latency = (end - start) / (2 * N)
-    print("Latency:", latency)
+    data = ('abc', 123)
+    comm.send(data, dest=1)
+elif rank == 1:
+    data = comm.recv(source=0)
+    print(data)
+else:
+    print("Expected only two nodes")
